ffabb71ac7acc6ba34bd451190b115e4c67dd526
erikn775 <erikn775@gmail.com>
2021-05-27 08:39:11 -0600

    adding redux

--- src/components/CalculatorLogic.js
+++ src/components/CalculatorLogic.js
@@ -5,7 +5,8 @@ export default class CalculatorLogic extends React.Component {
     state = {
         income: null
     }
-    
+
+
     render(){
         return(
             <div>
@@ -31,4 +32,21 @@ switch (getTaxBracket(income))
                 totalTaxes += (Math.min(income, 40126) - 9876) * .12;
         }
 
+    }
+
+    function getTaxBracket(grossIncome){
+        if(grossIncome <= 9875)
+            return TaxBracket.Bracket0;
+        if(grossIncome > 9875 && grossIncome <= 40125)
+            return TaxBracket.Bracket12;
+        if(grossIncome > 40125 && grossIncome <= 85525)
+            return TaxBracket.Bracket22;
+        if(grossIncome > 85525 && grossIncome <= 163300)
+            return TaxBracket.Bracket24;
+        if(grossIncome > 163300 && grossIncome <= 207350)
+            return TaxBracket.Bracket32;
+        if(grossIncome > 207350 && grossIncome <= 518400)
+            return TaxBracket.Bracket35;
+
+        return TaxBracket.Bracket37;
     }
