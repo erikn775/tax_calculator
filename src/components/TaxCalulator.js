0cdcca1b5fae29639a0a1df9d027709457d0f005
erikn775 <erikn775@gmail.com>
2021-05-27 09:32:01 -0600

    changed tax calculator to class component

--- src/components/TaxCalulator.js
+++ src/components/TaxCalulator.js
@@ -1,43 +1,54 @@
 import React, {useState, useEffect} from 'react';
-import {Form, Button} from 'react-bootstrap'
+import {Form, Button} from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { addIncome } from '../actions/TaxActions'
 
-export default function TaxCalculator() {
-    const [income, setIncome] = useState(0);
-    const [netIncome, setNetIncome] = useState(0);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        
+class TaxCalculator extends React.Component {
+
+    state = {
+        income: 0
     }
-
-    const counter = useSelector(state => {
-        console.log(state)
-        
-    });
     
-    const dispatch = useDispatch();
-
-    const handleChange = (event) => {
-        setIncome(event.target.value)
+    handleSubmit = (event) => {
+        let income = this.state.income
+        this.props.addIncome(income)
     }
 
-    return(
-        <div>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group controlId="income">
-                    <Form.Label>Gross Income</Form.Label>
-                    <Form.Control type="text" onChange={handleChange}/>
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+    handleChange = (event) => {
+        this.setState({
+            income: event.target.value
+        })
+    }
 
+    render(){
+        return(
             <div>
-                <h3>Net Income:</h3>
-                {netIncome}
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group controlId="income">
+                        <Form.Label>Gross Income</Form.Label>
+                        <Form.Control type="text" onChange={this.handleChange}/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+    
+                <div>
+                    <h3>Net Income:</h3>
+                    {this.state.income}
+                </div>
+    
             </div>
-
-        </div>
-    )
+        )
+    }
+    
 }
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addIncome: (income) => dispatch(addIncome(income))
+        }
+  }
+
+export default connect(null, mapDispatchToProps)(TaxCalculator);
--- src/reducers/TaxReducer.js
+++ src/reducers/TaxReducer.js
@@ -1,19 +1,19 @@
 
-export default function TaxReducer(state = {income: 0, totalTaxes: 0}, action){
+export default function TaxReducer(state = {totalTaxes: 0}, action){
     switch (action.type)
     {
         case "Bracket37":
-            totalTaxes += (income - 518401) * .37;
-        case "Bracket35":
-            totalTaxes += (Math.min(income, 518400) - 207351) * .35;
-        case "Bracket32":
-            totalTaxes += (Math.min(income, 207351) - 163301) * .32;
-        case "Bracket24":
-            totalTaxes += (Math.min(income, 163301) - 85526) * .24;
-        case "Bracket22":
-            totalTaxes += (Math.min(income, 85526) - 40126) * .22;
-        case "Bracket12":
-            totalTaxes += (Math.min(income, 40126) - 9876) * .12;
+            state.totalTaxes += (action.payload - 518401) * .37;
+        // case "Bracket35":
+        //     totalTaxes += (Math.min(income, 518400) - 207351) * .35;
+        // case "Bracket32":
+        //     totalTaxes += (Math.min(income, 207351) - 163301) * .32;
+        // case "Bracket24":
+        //     totalTaxes += (Math.min(income, 163301) - 85526) * .24;
+        // case "Bracket22":
+        //     totalTaxes += (Math.min(income, 85526) - 40126) * .22;
+        // case "Bracket12":
+        //     totalTaxes += (Math.min(income, 40126) - 9876) * .12;
     }
 }
 
