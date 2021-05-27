4bb5322401dee81a22dc88783320e366b62b1028
erikn775 <erikn775@gmail.com>
2021-05-27 08:54:39 -0600

    added tax reducer

---
+++ src/actions/TaxActions.js
@@ -0,0 +1,18 @@
+
+
+export const addIncome = (income) => {
+    if(income <= 9875)
+        return {type: "Bracket0", payload: income}
+    if(income > 9875 && income <= 40125)
+        return {type: "Bracket12", payload: income}
+    if(income > 40125 && income <= 85525)
+        return {type: "Bracket22", payload: income}
+    if(income > 85525 && income <= 163300)
+        return {type: "Bracket24", payload: income}
+    if(income > 163300 && income <= 207350)
+        return {type: "Bracket32", payload: income}
+    if(income > 207350 && income <= 518400)
+        return {type: "Bracket35", payload: income}
+    if(income < 518400)
+        return {type: "Bracket37", payload: income}
+}
---
+++ src/reducers/TaxReducer.js
@@ -0,0 +1,18 @@
+
+
+switch (action.type)
+    {
+        case "Bracket37":
+            totalTaxes += (income - 518401) * .37;
+        case "Bracket35":
+            totalTaxes += (Math.min(income, 518400) - 207351) * .35;
+        case "Bracket32":
+            totalTaxes += (Math.min(income, 207351) - 163301) * .32;
+        case "Bracket24":
+            totalTaxes += (Math.min(income, 163301) - 85526) * .24;
+        case "Bracket22":
+            totalTaxes += (Math.min(income, 85526) - 40126) * .22;
+        case "Bracket12":
+            totalTaxes += (Math.min(income, 40126) - 9876) * .12;
+    }
+    
